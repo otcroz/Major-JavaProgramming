ace877a6e89f51bd3845e501ba35847b6a9dc47b
yxxyn20 <dbtndus0527@gmail.com>
2021-09-30 18:26:32 +0900

    예제 코드 해설 후 수정 1. 구슬을 얻는 과정의 메서드를 얻는 행위, 잃는 행위로 나눔 2. BeadingTest 클래스에서 main함수 밖에 참조변수(instant) 선언 후,  main 함수(static) 내에 다시 test클래스를 선언하여 main 함수 밖에 있는 참조 변수를 main 함수에서 사용할 수 있도록 함. -> test 객체가 main 안에 생성되면서 참조변수가 생성되었음 -> instant 변수 사용 가능 3. Beading 클래스에 show() 메서드 추가

--- Chapter04/Beading/src/BeadingTest.java
+++ Chapter04/Beading/src/BeadingTest.java
@@ -1,14 +1,32 @@
+import exam.bead.Child;
 
 public class BeadingTest {
 
+	Child c1, c2; //참조값을 갖는 변수
+	
+	void show() {
+		System.out.print("어린이1 ");
+		c1.show();
+		System.out.print("어린이2 ");
+		c2.show();
+	}
+	
 	public static void main(String[] args) {
-		Child c1 = new Child(15);
-		Child c2 = new Child(9);
 		
-		c1.Beading(c2, 2);
-		c2.Beading(c1, 7);
+		BeadingTest test = new BeadingTest(); //static인 main 함수에서 c1, c2를 사용하기 위해 선언
 		
-		System.out.println("아이 1의 구슬 개수: "+ c1.getBead());
-		System.out.println("아이 2의 구슬 개수: "+ c2.getBead());
+		test.c1 = new Child(15);
+		test.c2 = new Child(9);
+		
+		System.out.println(">> 게임 전 보유 개수");
+		test.show();
+		
+		System.out.println("\n>> 1차 게임");
+		test.c1.obtainBead(test.c2, 2);
+		test.show();
+		
+		System.out.println("\n>> 2차 게임");
+		test.c2.obtainBead(test.c1, 7);
+		test.show();
 	}
 }
--- Chapter04/Beading/src/Child.java
+++ Chapter04/Beading/src/Child.java
@@ -1,7 +1,8 @@
 
+
 public class Child {
 	private int beadNum;
-	
+
 	// 생성자
 	public Child() {
 		this(0);
@@ -12,17 +13,25 @@ public class Child {
 	}
 
 	// 메서드
-	public void Beading(Child c, int n) {
-		if (c.beadNum > n) {
-			this.beadNum += n;
-			c.beadNum -= n;
-		} else {
-			this.beadNum += c.beadNum;
-			c.beadNum = 0;
+	public void obtainBead(Child c, int nObtainBead) {
+		beadNum += c.loseBead(nObtainBead);
+	}
+
+	public int loseBead(int nLoseBead) {
+		if (beadNum < nLoseBead) {
+			int rBead = beadNum;
+			beadNum = 0;
+			return rBead;
 		}
+		beadNum -= nLoseBead;
+		return nLoseBead;
 	}
 
 	public int getBead() {
 		return beadNum;
+	}
+
+	public void show() {
+		System.out.println("보유 개수:" + beadNum);
 	}
 }
