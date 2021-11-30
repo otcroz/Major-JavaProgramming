55787849db1c4193a67d930713fd7e3584193644
yxxyn20 <dbtndus0527@gmail.com>
2021-11-30 23:48:41 +0900

    JtextField 이벤트 설정 -> actionEvent

--- Final_Book_JAVAProgramming/ch11_SwingComponent/src/ex11_9/TextAreaEx.java
+++ Final_Book_JAVAProgramming/ch11_SwingComponent/src/ex11_9/TextAreaEx.java
@@ -1,8 +1,11 @@
 package ex11_9;
 
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -21,13 +24,25 @@ public class TextAreaEx {
 	
 	private void buildGUI() {
 		Panel panel = new Panel();
+		JLabel label = new JLabel("입력 후 <Enter>을 입력하세요");
 		JTextField field = new JTextField(20);
 		JTextArea area = new JTextArea(7,20);
 		
-		
+		field.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JTextField t = (JTextField)e.getSource();
+				area.append(t.getText() + "\n");
+				
+				t.setText("");
+			}
+			
+		});
+		panel.add(label);
 		panel.add(field);
 		panel.add(area);
 		
-		
+		frame.add(panel);
 	}
 }
